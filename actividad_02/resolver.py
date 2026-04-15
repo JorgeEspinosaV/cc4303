151880dd67dd2621639fff7f293fb24597cce2e5
Esperanza Cares <esperanza.cares@ug.uchile.cl>
2026-04-15 16:16:48 -0400

    paso2 completo

--- actividad_02/resolver.py
+++ actividad_02/resolver.py
@@ -1,5 +1,42 @@
 import socket
-from dnslib import DNSRecord
+from dnslib import DNSRecord, QTYPE
+def parse_dns_message(data):
+    d = DNSRecord.parse(data)
+    qname = str(d.get_q().get_qname())
+    ancount = d.header.a
+    nscount = d.header.auth
+    arcount = d.header.ar
+    parsed_message = {
+        "Qname": qname,
+        "ANCOUNT": ancount,
+        "NSCOUNT": nscount,
+        "ARCOUNT": arcount,
+        "Answer": [],
+        "Authority": [],
+        "Additional": [],
+    }
+
+    for rr in d.rr:
+        parsed_message["Answer"].append({
+            "name": str(rr.rname),
+            "type": QTYPE[rr.rtype],
+            "data": str(rr.rdata)
+        })
+
+    for rr in d.auth:
+        parsed_message["Authority"].append({
+            "name": str(rr.rname),
+            "type": QTYPE[rr.rtype],
+            "data": str(rr.rdata)
+        })
+    for rr in d.ar:
+        parsed_message["Additional"].append({
+            "name": str(rr.rname),
+            "type": QTYPE[rr.rtype],
+            "data": str(rr.rdata)
+        })
+    return parsed_message
+    
 
 #creamos socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,12 +45,6 @@ while True:
     data, addr = sock.recvfrom(4096)
     print("Mensaje recibido desde:", addr)
     print(data)
+    parsed = parse_dns_message(data)
+    print(parsed)
 
-def parse_dns_message(data):
-    d = DNSRecord.parse(data)
-
-    ancount = d.header.a
-    nscount = d.header.auth
-    arcount = d.header.ar
-
-    qname = str(d.get_q().get_qqname())
